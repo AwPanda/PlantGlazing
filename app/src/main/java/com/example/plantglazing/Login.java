c7c550fd0b2746f5841cd902a8f01c09927a9fb8
AwPanda <AwPanda@users.noreply.github.com>
2019-12-05 22:40:08 +0000

    Setup up login/register system

--- app/src/main/java/com/example/plantglazing/Login.java
+++ app/src/main/java/com/example/plantglazing/Login.java
@@ -1,18 +1,14 @@
 package com.example.plantglazing;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.widget.Toast;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity  {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_login);
     }
 }
